bc3ea396e87d4ea0e8acbccca631073ead378905
david.chapman <david.chapman@gridworks-ic.org>
2019-11-11 23:48:37 -0800

    Updates

---
+++ precache-manifest.8863b1b001407fe174ab6d8df8c42b57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a42c98796ee1bdb0ed67c6945a141c",
+    "url": "/responsive-magic-counting-tables/index.html"
+  },
+  {
+    "revision": "70d6f5c273e67107f378",
+    "url": "/responsive-magic-counting-tables/static/css/main.eb415faa.chunk.css"
+  },
+  {
+    "revision": "7434bf3b705d777495b0",
+    "url": "/responsive-magic-counting-tables/static/js/2.d87b9f8c.chunk.js"
+  },
+  {
+    "revision": "70d6f5c273e67107f378",
+    "url": "/responsive-magic-counting-tables/static/js/main.a211cf75.chunk.js"
+  },
+  {
+    "revision": "1b7570b61ea2a601dc19",
+    "url": "/responsive-magic-counting-tables/static/js/runtime-main.962cb927.js"
+  }
+]);
